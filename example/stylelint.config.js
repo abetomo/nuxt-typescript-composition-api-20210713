335e44e3b1000ad21be07572d0e822e75ecaf771
abetomo <abe@enzou.tokyo>
2022-12-22 10:51:04 +0900

    chore(deps): upgrade stylelint

--- example/stylelint.config.js
+++ example/stylelint.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+  customSyntax: 'postcss-scss',
   extends: [
     'stylelint-config-standard',
     'stylelint-config-prettier'
+  ],
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      customSyntax: '@stylelint/postcss-css-in-js',
+    },
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
